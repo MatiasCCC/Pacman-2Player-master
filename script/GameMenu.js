892916bb0fac7c7830e34d9d3a1aaba6849a7d89
MatiasC98 <83857707+MatiasC98@users.noreply.github.com>
2024-04-25 18:20:58 -0300

    intento GameMenu

--- script/GameMenu.js
+++ script/GameMenu.js
@@ -5,7 +5,12 @@ class GameMenu {
     this.currentSelectedMenu = GAME_STATE.PLAYER_VS_PLAYER;
     this.eventAdded = false;
     
-
+    // Image selection states for players
+    this.player1ImageSelection = 0;
+    this.player2ImageSelection = 0;
+    // Arrays of images for each player's selections
+    this.player1Images = ['image/p11.png', 'image/p12.png', 'image/p13.png'];
+    this.player2Images = ['image/p21.png', 'image/p22.png', 'image/p23.png'];
     
 
     this.addGameMenuControls = (e) => {
@@ -22,6 +27,22 @@ class GameMenu {
         if (this.currentSelectedMenu != GAME_STATE.PLAYER_VS_PLAYER) {
           this.currentSelectedMenu = this.currentSelectedMenu + 100;
         }
+      }
+        // Player 1 cycles images with left and right keys
+      if (e.keyCode === PLAYER1_CONTROL_KEY.RIGHT) {
+        this.player1ImageSelection = (this.player1ImageSelection + 1) % this.player1Images.length;
+        this.draw();
+      } else if (e.keyCode === PLAYER1_CONTROL_KEY.LEFT) {
+        this.player1ImageSelection = (this.player1ImageSelection - 1 + this.player1Images.length) % this.player1Images.length;
+        this.draw();
+      }
+      // Player 2 cycles images with left and right keys
+      if (e.keyCode === PLAYER2_CONTROL_KEY.RIGHT) {
+        this.player2ImageSelection = (this.player2ImageSelection + 1) % this.player2Images.length;
+        this.draw();
+      } else if (e.keyCode === PLAYER2_CONTROL_KEY.LEFT) {
+        this.player2ImageSelection = (this.player2ImageSelection - 1 + this.player2Images.length) % this.player2Images.length;
+        this.draw();
       }
     };
   }
@@ -43,6 +64,18 @@ class GameMenu {
   draw() {
     this.drawGameOptions();
     this.drawSelectionHighlighter();
+      // Draw Player 1's selected image
+    this.drawPlayerImage(this.player1Images[this.player1ImageSelection], 100, 100); // Example coordinates
+      // Draw Player 2's selected image
+    this.drawPlayerImage(this.player2Images[this.player2ImageSelection], 300, 100); 
+  }
+
+  drawPlayerImage(imagePath, x, y) {
+    let img = new Image();
+    img.onload = () => {
+      this.ctx.drawImage(img, x, y);
+    };
+    img.src = imagePath;
   }
 
   drawSelectionHighlighter() {
@@ -78,11 +111,7 @@ class GameMenu {
     this.ctx.fillStyle = '#000000';
     this.ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     
-
     //draw logo
-
-
-    
     this.ctx.drawImage(
       PACMAN_LOGO_IMAGE,
       0,
@@ -94,7 +123,6 @@ class GameMenu {
       CANVAS_WIDTH,
       200
     );
-
     //write game menu options
     //writeTextOnCanvasWithSize(this.ctx, 'Single Player Mode', 15, 'white', 20, 250);
     writeTextOnCanvasWithSize(this.ctx, '         EMPEZAR', 15, 'white', 20, 315);
